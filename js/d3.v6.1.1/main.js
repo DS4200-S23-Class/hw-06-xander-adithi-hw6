3c2b07ab4965297cb9ffafe372fa31dae34dbf95
Adithi Subramaniam <adithisubramaniam@Adithis-MacBook-Pro.local>
2023-02-27 18:28:55 -0500

    adding all updates

--- js/d3.v6.1.1/main.js
+++ js/d3.v6.1.1/main.js
@@ -15,33 +15,7 @@ const FRAME1 = d3.select("#vis1")
 // read scatter plot data
 d3.csv("data/iris.csv").then((DATA) => {
 
-    // // Scaling constants
-    // const X_SCALE = d3.scaleLinear()
-    //     .domain([0, 10])
-    //     .range([0, VIS_WIDTH]);
-    // const Y_SCALE = d3.scaleLinear()
-    //     .domain([10, 0])
-    //     .range([0, VIS_HEIGHT]);
 
-	// // Plots each of the data points
-	// FRAME1.selectAll("points")
-    //     .data(DATA)
-    //     .enter()
-    //     .append("circle")
-    //     .attr("cx", (d) => { return (X_SCALE(d.x) + MARGINS.left); })
-    //     .attr("cy", (d) => { return (Y_SCALE(d.y) + MARGINS.top) ; })
-    //     .attr("r", 10)
-    //     .attr("class", "point");
-
-	// // Add both axis' to vis1
-	// FRAME1.append("g")
-	// 	.attr("transform", "translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")")
-	// 	.call(d3.axisBottom(X_SCALE).ticks(10))
-    //     .attr("font-size", "15px");
-	// FRAME1.append("g")
-	// 	.attr("transform", "translate(" + MARGINS.left + "," + (MARGINS.bottom) + ")")
-	// 	.call(d3.axisLeft(Y_SCALE).ticks(10))
-    //     .attr("font-size", "15px");
 });
 
 
@@ -53,7 +27,7 @@ const FRAME2 = d3.select("#vis2")
     .attr("class", "frame");     
 
     
-// create svg in vis2 div
+// create svg in vis3 div
 const FRAME3 = d3.select("#vis3")
 .append("svg")
 .attr("height", FRAME_HEIGHT)
